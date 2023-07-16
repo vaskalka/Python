bf444608ae39b315c6971c6500a4a01007f98df8
Василий Сторожев <vvstorozhev@bk.ru>
2023-07-16 14:30:59 +0300

    Hometasks done!

---
+++ Test_Project/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
